b4a1834aec6e3a0f86271cc41a2f16cfe6380ad4
nivedhanr <nivedharenahan@gmail.com>
2022-11-21 12:24:56 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a,b,c,result;
+        Scanner s=new Scanner(System.in);
+        System.out.println("1.Find largest number\n2.Find smallest number\n3.Prime number or Not\n4.Even or Not\n5.Find reverse");
+        System.out.println("Enter your choice:");
+        a=s.nextInt();
+
+    }
+
+}
